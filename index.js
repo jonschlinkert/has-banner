bc888d9651eb9e511b78ce873725d19919f3457f
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-20 17:37:06 -0400

    refactor

--- index.js
+++ index.js
@@ -7,15 +7,39 @@
 
 'use strict';
 
-var bom = require('strip-bom');
+var strip = require('strip-use-strict');
+var bom = require('strip-bom-string');
 
-module.exports = function(str, arr) {
+module.exports = function(str, custom) {
   if (typeof str !== 'string') {
     throw new Error('has-banner expects a string.');
   }
 
-  str = bom(str).replace(/^\s+/, '');
-  var re = new RegExp(['global', 'jshint'].concat(arr || []).join('|'));
+  str = strip(bom(str)).replace(/^\s+/, '');
+  var start = /^(\/[*!]+|\/\/)/.exec(str);
+  if (!start) {
+    return false;
+  }
 
-  return str.indexOf('/*') === 0 && !re.test(str);
+  var names = ['global', 'jshint', 'eslint'];
+  if (Array.isArray(custom)) {
+    names = names.concat(custom);
+  }
+
+  var chars = start[0];
+  str += '\n';
+
+  var end = chars === '//'
+    ? str.indexOf('\n')
+    : str.indexOf('*/');
+
+  if (end === -1) {
+    return false;
+  }
+
+  var comment = str.slice(chars.length, end);
+  comment = comment.replace(/^[\s\W]+|[\s\W]+$/g, '');
+
+  var re = new RegExp('^' + names.join('|') + '[-: \\t]?');
+  return !re.test(comment);
 };
